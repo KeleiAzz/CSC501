4b52fc3a2faebfe6d6502e73e3887bde1a6ddb34
KeleiAzz <kgong@ncsu.edu>
2015-12-03 23:25:18 -0500

    workable version

--- csc501-lab3/sys/lock.c
+++ csc501-lab3/sys/lock.c
@@ -11,18 +11,29 @@ int lock (int ldes1, int type, int priority)
 	int lock_id = ldes1 % NLOCKS;
 
 	disable(ps);
-	if(isbadlock(lock_id) || locks[lock_id].lstate == LFREE )
+	if(isbadlock(lock_id) || locks[lock_id].lstate == LFREE || ldes1 != locks[lock_id].lockdes)
 	{
 		restore(ps);
 		return SYSERR;
 	}
-	lptr = &locks[lock_id];
+	// lptr = &locks[lock_id];
 
-	if( lptr -> lreaders == 0 && lptr -> lwriters == 0) should_wait = 0;
-	else if( lptr -> lreaders > 0 && lptr -> lwriters == 0)
+	if( locks[lock_id].lreaders == 0 && locks[lock_id].lwriters == 0) should_wait = 0;
+	else if( locks[lock_id].lreaders > 0 && locks[lock_id].lwriters == 0)
 	{
 		if(type == READ){
+			// if a writer with higher priority is waiting, then this reader should wait.
 			should_wait = 0;
+			int item;
+			for(item = q[locks[lock_id].lqtail].qprev; item != locks[lock_id].lqhead && q[item].qkey > priority; item = q[item].qprev)
+			{
+				if(q[item].qtype == WRITE )
+				{
+					should_wait = 1;
+					break;
+				}
+			}
+			
 		}else
 		{
 			should_wait = 1;
@@ -32,21 +43,25 @@ int lock (int ldes1, int type, int priority)
 	{
 		should_wait = 1;
 	}
-	kprintf("currpid %d, type: %d should wait: %d\n", currpid, type, should_wait);
+	// kprintf("ldes %d, currpid %d, type: %d should wait: %d, readers: %d, writers: %d\n", ldes1, currpid, type, should_wait, lptr -> lreaders, lptr -> lwriters);
 	if(should_wait)
 	{
-		proctab[currpid].pstate = PRLWAIT;
-		proctab[currpid].plwaitret = OK;
-		insert(currpid, lptr -> lqhead, priority);
-		q[ldes1].qtype = type;
+		// int pid = currpid;
+		struct  pentry  *pptr;
+		pptr = &proctab[currpid];
+		pptr -> pstate = PRLWAIT;
+		pptr -> plwaitret = OK;
+		insert(currpid, locks[lock_id].lqhead, priority);
+		q[currpid].qtype = type;
 		resched();
 		restore(ps);
-		return proctab[currpid].plwaitret;
+		// kprintf("plwaitret: %d\n", pptr -> plwaitret);
+		return pptr -> plwaitret;
 	}else
 	{
 		lock_tab[currpid][lock_id] ++;
-		if(type == READ) lptr -> lreaders ++;
-		else if(type == WRITE) lptr -> lwriters ++;
+		if(type == READ) locks[lock_id].lreaders ++;
+		else if(type == WRITE) locks[lock_id].lwriters ++;
         restore(ps);
        	return(OK);
 	}
--- csc501-lab3/sys/releaseall.c
+++ csc501-lab3/sys/releaseall.c
@@ -7,12 +7,88 @@ int releaseall(int numlocks, int ldes1, ...)
 {
 	int i;
 	int lockdes, has_err = 0;
+	int pid = currpid;
+	STATWORD ps;
+	disable(ps);
+	struct lentry *lptr;
 	// unsigned long *a = (unsigned long *)(&ldes1);
     for (i = 0 ; i < numlocks ; i++)
 	{
         lockdes = *(&ldes1 + i); 
-		if(release(currpid, lockdes) == SYSERR) has_err = 1;
+        // kprintf("release lockdes: %d\n", lockdes);
+		int lock_id = lockdes % NLOCKS;
+		lptr = &locks[lock_id];
+		if(isbadlock(lock_id) || lptr -> lstate == LFREE )
+		{
+			// restore(ps);
+			has_err++;
+			continue;
+			// return SYSERR;
+		}
+
+		if( lock_tab[pid][lock_id] > 0)
+			lock_tab[pid][lock_id] --;
+		else
+		{
+			// restore(ps);
+			has_err++;
+			continue;
+			// kprintf("something goes wrong\n");
+			// return SYSERR;
+		}
+
+		if(lptr -> lreaders > 0 && lptr -> lwriters == 0)
+			lptr -> lreaders --;
+		else if(lptr -> lreaders == 0 && lptr -> lwriters == 1)
+			lptr -> lwriters --;
+	
+	// kprintf("currpid: %d, readers: %d, writers: %d\n", currpid, lptr -> lreaders, lptr -> lwriters);
+		if(lptr -> lreaders == 0 && lptr -> lwriters == 0)
+		{
+		//need to turn some process to ready
+			int last, item, last_key, tmp;
+			last = q[lptr -> lqtail].qprev;
+		
+			if( last == lptr -> lqhead) 
+			{
+			// kprintf("empty queue\n");
+				continue;
+			}
+		// kprintf("last in queue type: %d\n",q[last].qtype);
+			if(q[last].qtype == READ)
+			{
+				lptr -> lreaders ++;
+				lock_tab[last][lock_id] ++;
+				last_key = q[last].qkey;
+				item = q[last].qprev;
+				dequeue(last);
+				ready(last, RESCHNO);
+				// kprintf("Last in q: %d, set ready\n", last);
+				while(item != lptr -> lqhead)
+				{
+					if(q[item].qtype == READ && q[item].qkey == last_key)
+					{
+						tmp = item;
+						item = q[item].qprev;
+						// kprintf("turn process %d to ready\n", tmp);
+						dequeue(tmp);
+						ready(tmp, RESCHNO);
+					}
+				}
+			}
+			else
+			{
+				lptr -> lwriters ++;
+				lock_tab[last][lock_id] ++;
+				dequeue(last);
+				ready(last, RESCHNO);
+			}
+
+		}
+		
+		// if(release(currpid, lockdes) == SYSERR) has_err = 1;
 	}
+	restore(ps);
 	resched();
 	if(has_err == 1) return(SYSERR);
 	else return(OK);
@@ -21,7 +97,7 @@ int releaseall(int numlocks, int ldes1, ...)
 
 int release(int pid, int lockdes)
 {
-	kprintf("release pid: %d, lockdes: %d\n", pid, lockdes);
+	// kprintf("release pid: %d, lockdes: %d\n", pid, lockdes);
 	STATWORD ps;
 	struct lentry *lptr;
 	int lock_id = lockdes % NLOCKS;
@@ -38,6 +114,7 @@ int release(int pid, int lockdes)
 	else
 	{
 		restore(ps);
+		// kprintf("something goes wrong\n");
 		return SYSERR;
 	}
 
@@ -46,7 +123,7 @@ int release(int pid, int lockdes)
 	else if(lptr -> lreaders == 0 && lptr -> lwriters == 1)
 		lptr -> lwriters --;
 	
-	kprintf("readers: %d, writers: %d\n", lptr -> lreaders, lptr -> lwriters);
+	// kprintf("currpid: %d, readers: %d, writers: %d\n", currpid, lptr -> lreaders, lptr -> lwriters);
 	if(lptr -> lreaders == 0 && lptr -> lwriters == 0)
 	{
 		//need to turn some process to ready
@@ -55,25 +132,26 @@ int release(int pid, int lockdes)
 		
 		if( last == lptr -> lqhead) 
 		{
-			kprintf("empty queue\n");
+			// kprintf("empty queue\n");
 			return OK;
 		}
-		kprintf("last in queue type: %d\n",q[last].qtype);
+		// kprintf("last in queue type: %d\n",q[last].qtype);
 		if(q[last].qtype == READ)
 		{
 			lptr -> lreaders ++;
-			lock_tab[pid][lock_id] ++;
+			lock_tab[last][lock_id] ++;
 			last_key = q[last].qkey;
 			item = q[last].qprev;
 			dequeue(last);
 			ready(last, RESCHNO);
-			kprintf("Last in q: %d, set ready\n", last);
+			// kprintf("Last in q: %d, set ready\n", last);
 			while(item != lptr -> lqhead)
 			{
 				if(q[item].qtype == READ && q[item].qkey == last_key)
 				{
 					tmp = item;
 					item = q[item].qprev;
+					// kprintf("turn process %d to ready\n", tmp);
 					dequeue(tmp);
 					ready(tmp, RESCHNO);
 				}
